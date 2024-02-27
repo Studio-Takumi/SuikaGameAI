2500c1174b8a9e5a71853d1e6d9a288f98164408
Studio-Takumi <iwataku328@gmail.com>
2024-02-27 13:41:14 +0900

    #2 Box2Dに乗り換え

--- SuikaGamev2.py
+++ SuikaGamev2.py
@@ -9,16 +9,18 @@ import pygame
 from pygame.locals import *
 from Box2D import b2
 class PhysicsObject():
-    def __init__(self,type,isFixed):
+    def __init__(self,type,isStatic):
         self.type = type
-        self.isFixed = isFixed
+        self.isStatic = isStatic
     def getType(self):
         return self.type
-    def getIsFixed(self):
-        return self.isFixed
-class FixedLine(PhysicsObject):
-    def __init__(self,type,isFixed,pos):
-        super().__init__(type,isFixed)
+    def getIsStatic(self):
+        return self.isStatic
+class StaticLine(PhysicsObject):
+    def __init__(self,type,isStatic,pos,world):
+        super().__init__(type,isStatic)
+        self.body = world.CreateStaticBody(position=(0, 0), 
+            shapes=b2.edgeShape(vertices=[(pos['X1']/size,pos['Y1']/size), (pos['X2']/size, pos['Y2']/size)]))
         self.pos1 = np.array([pos['X1'], pos['Y1']],dtype=np.float)
         self.pos2 = np.array([pos['X2'], pos['Y2']],dtype=np.float)
     def getX1(self):
@@ -33,94 +35,58 @@ class FixedLine(PhysicsObject):
         return self.pos1
     def getPos2(self):
         return self.pos2
-class FreeCircle(PhysicsObject):
-    def __init__(self, type, isFixed, mass, pos):
-        super().__init__(type,isFixed)
+class DynamicCircle(PhysicsObject):
+    def __init__(self, type, isStatic, mass, pos, world):
+        super().__init__(type,isStatic)
+        self.body = world.CreateDynamicBody(position=(pos['X']/size, pos['Y']/size), fixtures=b2.fixtureDef(shape=b2.circleShape(radius=pos['R']/size), density=1))
         self.pos = np.array([pos['X'], pos['Y']],dtype=np.float)
         self.r = pos['R']
         self.m = mass
         self.v = np.array([0,0],dtype=np.float)
         self.f = np.array([0,0],dtype=np.float)
     def getX(self):
-        return self.pos[0]
+        return self.body.transform.position[0]*size
     def getY(self):
-        return self.pos[1]
+        return self.body.transform.position[1]*size
     def getR(self):
         return self.r
-    def getMass(self):
-        return self.m
     def getPos(self):
         return self.pos
-    def getSpeed(self):
-        return self.v
-    def getForce(self):
-        return self.f
-    def setPos(self,Pos):
-        self.pos = Pos
-    def setSpeed(self,Speed):
-        self.v = Speed
-    def empower(self,force):
-        self.f += force
-    def reflect(self,deltaTime):
-        self.v   += self.f*deltaTime
-        self.pos += self.v*deltaTime
-        self.f = np.array([0,0],dtype=np.float)
-class FruitCircle(FreeCircle):
-    def __init__(self, type, isFixed, mass, pos, fruitID):
-        super().__init__(type, isFixed, mass, pos)
+class FruitCircle(DynamicCircle):
+    def __init__(self, type, isStatic, mass, pos, world, fruitID):
+        super().__init__(type, isStatic, mass, pos, world)
         self.fruitID = fruitID
     def getFruitID(self):
         return self.fruitID
-gravity = np.array([0,-100],dtype=np.float)
-elastic = 0.2
+size = 100
 class PhysicsEngine():
     def __init__(self) :
         self.objects = []
         self.world = b2.world(gravity=(0, -10), doSleep=True)
-    def createLine(self,isFixed,pos):
-        if isFixed == True:
-            self.objects.append(FixedLine(type='Line',isFixed=isFixed,pos=pos))
-    def createCircle(self,isFixed,pos):
-        if isFixed == False:
-            self.objects.append(FreeCircle(type='Circle',isFixed=isFixed,pos=pos))
+    def createLine(self,isStatic,pos,world):
+        if isStatic == True:
+            self.objects.append(StaticLine(type='Line',isStatic=isStatic,pos=pos,world=world))
+    def createCircle(self,isStatic,pos):
+        if isStatic == False:
+            self.objects.append(DynamicCircle(type='Circle',isStatic=isStatic,pos=pos))
     def getObjects(self):
         return self.objects
-    def getFixedLineObjects(self):
-        FixedLines = []
+    def getStaticLineObjects(self):
+        StaticLines = []
         GameObjects = self.getObjects()
         for GameObject in GameObjects:
-            if GameObject.getType() == 'Line' and GameObject.getIsFixed() == True:
-                FixedLines.append(GameObject)
-        return FixedLines
-    def getFreeCircleObjects(self):
-        FreeCircles = []
+            if GameObject.getType() == 'Line' and GameObject.getIsStatic() == True:
+                StaticLines.append(GameObject)
+        return StaticLines
+    def getDynamicCircleObjects(self):
+        DynamicCircles = []
         GameObjects = self.getObjects()
         for GameObject in GameObjects:
-            if GameObject.getType() == 'Circle' and GameObject.getIsFixed() == False:
-                FreeCircles.append(GameObject)
-        return FreeCircles
-        posA = CircleA.getPos()
-        posB = CircleB.getPos()
-        speedA = CircleA.getSpeed()
-        speedAnorm = np.linalg.norm(speedA)
-        speedB = CircleB.getSpeed()
-        speedBnorm = np.linalg.norm(speedB)
-        massA = CircleA.getMass()
-        massB = CircleB.getMass()
-        disAB = np.linalg.norm(posB-posA)
-        rotateAB = np.dot(posB-posA,np.array([[0,1],[-1,0]]))
-        if disAB <= CircleA.getR() + CircleB.getR():
-            print('Collision!!')
-            speedAa = (massA*speedA+massB*speedB - elastic*massB*(speedA-speedB)) / (massA+massB)
-            speedBb = (massA*speedA+massB*speedB - elastic*massA*(speedB-speedA)) / (massA+massB)
-            CircleA.setSpeed(speedAa)
-            CircleB.setSpeed(speedBb)
-            CircleA.setPos(posA + (posA-posB)*0.01)
-            CircleB.setPos(posB + (posB-posA)*0.01)
-            CircleA.empower((posA-posB)/disAB*massA*np.linalg.norm(speedAa-speedA)*5)
-            CircleB.empower((posB-posA)/disAB*massB*np.linalg.norm(speedBb-speedB)*5)
+            if GameObject.getType() == 'Circle' and GameObject.getIsStatic() == False:
+                DynamicCircles.append(GameObject)
+        return DynamicCircles
     def calc(self,deltaTime):
-        CircleObjects = self.getFreeCircleObjects()
+        CircleObjects = self.getDynamicCircleObjects()
                 
 offsetX,offsetY = 400,470
 dropLineLength,dropLineY = 400,440
@@ -133,22 +99,22 @@ class SuikaEngine(PhysicsEngine):
         self.createBascket()
         self.play()
     def createBascket(self):
-        self.createLine(isFixed=True,pos={'X1':200,'Y1':400,'X2':200,'Y2':0})
-        self.createLine(isFixed=True,pos={'X1':-200,'Y1':400,'X2':-200,'Y2':0})
-        self.createLine(isFixed=True,pos={'X1':200,'Y1':0,'X2':-200,'Y2':0})
+        self.createLine(isStatic=True,pos={'X1':200,'Y1':400,'X2':200,'Y2':0},world=self.world)
+        self.createLine(isStatic=True,pos={'X1':-200,'Y1':400,'X2':-200,'Y2':0},world=self.world)
+        self.createLine(isStatic=True,pos={'X1':200,'Y1':0,'X2':-200,'Y2':0},world=self.world)
     def createFruit(self,mouseX,mouseY):
         fruitID = self.preFruitID
         pos= {'X':mouseX,'Y':dropLineY,'R':fruitID*5+10}
-        self.objects.append(FruitCircle(type='Circle',isFixed=False,mass=1,pos=pos,fruitID=fruitID))
+        self.objects.append(FruitCircle(type='Circle',isStatic=False,mass=1,pos=pos,world=self.world,fruitID=fruitID))
         self.preFruitID = random.randint(6,6)
     def render(self,screen,mouseX,mouseY):
-        for Line in self.getFixedLineObjects():
+        for Line in self.getStaticLineObjects():
             pygame.draw.line(screen, (255,219,90),
                 (offsetX+Line.getX1(),offsetY-Line.getY1()),
                 (offsetX+Line.getX2(),offsetY-Line.getY2()),1)
             pygame.draw.circle(screen,(255,219,90),(offsetX+Line.getX1()+1,offsetY-Line.getY1()+1),1)
             pygame.draw.circle(screen,(255,219,90),(offsetX+Line.getX2()+1,offsetY-Line.getY2()+1),1)
-        for Fruit in self.getFreeCircleObjects():
+        for Fruit in self.getDynamicCircleObjects():
             pygame.draw.circle(screen,FruitsColor[Fruit.getFruitID()],(offsetX+Fruit.getX()+1,offsetY-Fruit.getY()),Fruit.getR())
         pygame.draw.line(screen, (255,247,219),
             (offsetX+mouseX,offsetY-dropLineY),
@@ -174,7 +140,7 @@ class SuikaEngine(PhysicsEngine):
                     self.createFruit(mouseX,mouseY)
             deltaTime = preTime - time.time()
             preTime = time.time()
-            self.calc(deltaTime)
+            self.world.Step(1/100, 10, 8)
             screen.fill((255,243,200))
             screen = self.render(screen,mouseX,mouseY)
             pygame.display.update()
